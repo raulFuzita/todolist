9c7e319544a0cc877638180f3ee7e8ee7e2c2c6d
raulFuzita <raul_fuzita@hotmail.com>
2020-12-29 19:41:08 +0000

    Folder database added. package.json is added. File app added

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
