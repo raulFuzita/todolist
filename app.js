eae3bade244b1c271f652ea6f53392c58a7d9a57
raulFuzita <raul_fuzita@hotmail.com>
2020-12-29 20:02:29 +0000

    Controllers were imported to ap.js. Routes were created

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const session = require('express-session');
 const morgan = require('morgan');
+const userController = require('./controllers/user/userController');
+const logoutController = require('./controllers/logout/logoutController');
+const todolistController = require('./controllers/task/taskController');
 
 const app = express();
 
@@ -29,6 +32,16 @@ app.get('/', (req, res) => {
 app.get('/index', (req, res) => {
     res.render('index', {session: req.session});
 });
+
+app.get('/login', userController.user_login_get);
+app.post('/login', userController.user_login_post);
+app.get('/logout', logoutController.logout);
+app.get('/signup', userController.user_signup_get);
+app.post('/signup', userController.user_signup_post);
+app.get('/todolist', todolistController.task_index_get);
+app.post('/create', todolistController.task_create_post);
+app.post('/update', todolistController.task_update_post);
+app.post('/delete', todolistController.task_delete_post);
 
 app.use((req, res) => {
     res.status(404).render('404', {session: req.session});
