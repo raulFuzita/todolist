cb0208a9947f607b30c416cc1d173e98557eeb33
raulFuzita <raul_fuzita@hotmail.com>
2020-12-29 19:46:53 +0000

    Installed: express-session (session), morgan (middleware), ejs (template engine).

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const session = require('express-session');
+const morgan = require('morgan');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();              
+});
+
+app.use(session({
+    secret: 'todolist',
+    resave: false,
+    saveUninitialized: true
+}));
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
