abc9fd14e461c22892459bfb93a2e790bb8fca2c
raulFuzita <raul_fuzita@hotmail.com>
2021-04-27 16:47:40 +0100

    Password comparison feedback (it doesn't support color blind yet)

--- public/assets/js/toolbox.js
+++ public/assets/js/toolbox.js
@@ -8,3 +8,26 @@ const toggleShowPassword = (iconId, inpuId) => {
         $(iconId).removeClass('fa-eye').addClass('fa-eye-slash')
     }
 }
+
+const comparePassword = (confirmInput, passwordInput) => {
+    $(confirmInput).keyup(() => {
+
+        if ($(confirmInput).val() && $(passwordInput).val()) {
+            if ($(confirmInput).val() == $(passwordInput).val()){
+                $(confirmInput).css('color', 'green')
+                $(passwordInput).css('color', 'green')
+            } else {
+                $(confirmInput).css('color', 'red')
+                $(passwordInput).css('color', 'red')
+            }
+        } else {
+            $(confirmInput).css('color', 'initial')
+            $(passwordInput).css('color', 'initial')
+        }
+    })
+}
+
+$('.clear-input').click((e) => {
+    $(e.target).parent().prev('input').val('')
+})
+
